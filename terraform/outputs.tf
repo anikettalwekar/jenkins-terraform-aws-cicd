b4052b9c5cf72d8f065efdb2897ee5446de8275b
anikettalwekar <talwekaraniket.11@gmail.com>
2025-11-11 11:43:46 +0530

    updated file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.main_vpc.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.main_subnet.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.infra_project_sg.id
+}
+
 output "instance_id" {
   value = aws_instance.demo_server.id
 }
@@ -6,6 +18,3 @@ output "instance_public_ip" {
   value = aws_instance.demo_server.public_ip
 }
 
-output "vpc_id" {
-  value = aws_vpc.main_vpc.id
-}
