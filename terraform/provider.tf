9d303566c49aadc1dbe65a5c1f57d070812ee139
anikettalwekar <talwekaraniket.11@gmail.com>
2025-11-11 09:45:00 +0530

    removed S3 backend, using local state instead

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,13 +2,4 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "aniket-tf-state-2025"
-    key            = "jenkins-cicd/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-  }
-}
-
 
