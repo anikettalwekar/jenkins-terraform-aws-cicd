afc2774cb8970da47ceb54b0d642664e704bca1e
anikettalwekar <talwekaraniket.11@gmail.com>
2025-11-11 11:29:56 +0530

    resources name chnaged

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 
-  tags = { Name = "jenkins-vpc" }
+  tags = { Name = "infra-project-vpc" }
 }
 
 resource "aws_subnet" "main_subnet" {
@@ -11,12 +11,12 @@ resource "aws_subnet" "main_subnet" {
   map_public_ip_on_launch = true
   availability_zone       = "ap-south-1a"
 
-  tags = { Name = "jenkins-subnet" }
+  tags = { Name = "infra-project-subnet" }
 }
 
-resource "aws_security_group" "jenkins_sg" {
+resource "aws_security_group" "infra_project_sg" {
   vpc_id = aws_vpc.main_vpc.id
-  name   = "jenkins-sg"
+  name   = "infra-project-sg"
 
   ingress {
     from_port   = 22
@@ -39,7 +39,7 @@ resource "aws_security_group" "jenkins_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = { Name = "jenkins-sg" }
+  tags = { Name = "infra-project-sg" }
 }
 
 resource "aws_instance" "demo_server" {
@@ -47,7 +47,7 @@ resource "aws_instance" "demo_server" {
   instance_type          = "t2.medium"
   key_name               = var.key_name
   subnet_id              = aws_subnet.main_subnet.id
-  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  vpc_security_group_ids = [aws_security_group.infra_project_sg.id]
 
-  tags = { Name = "jenkins-demo-ec2" }
+  tags = { Name = "infra-project-ec2" }
 }
